b25f36431e4c1b5e9e5cc658729e7a5b4992b889
maniac-b8 <matthewdixon2539@gmail.com>
2024-05-13 12:09:14 -0700

    add ensureLoggedIn to routes that should have them

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const upload = require("multer")();
 const postsCtrl = require('../../controllers/api/posts');
+const ensureLoggedIn = require ('../../config/ensureLoggedIn')
 
 // All routes start with ("/api/posts")
 
 // GET /api/posts
 router.get('/', postsCtrl.index);
 // POST /api/posts/upload
-router.post('/upload', upload.single('photo'), postsCtrl.upload);
+router.post('/upload', ensureLoggedIn, upload.single('photo'), postsCtrl.upload);
 // DELETE /api/posts/postId
-router.delete('/:id', postsCtrl.remove);
+router.delete('/:id', ensureLoggedIn, postsCtrl.remove);
 // GET /api/posts/postId
 router.get('/:id', postsCtrl.getPhoto);
 
